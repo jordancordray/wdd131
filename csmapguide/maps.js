cf84108706f880063e6a67935ba99b29446c31e2
jordancordray <cor23024@byui.edu>
2024-07-18 12:20:51 -0600

    final site

--- csmapguide/maps.js
+++ csmapguide/maps.js
@@ -3,8 +3,8 @@ const maps = [
         mapIndex: 0,
         mapName: 'Anubis',
         mapDesc: 'Anubis is an Egyptian-themed map characterized by its intricate temple architecture and ancient ruins. It features two bomb sites, A and B, with Site A located in an open courtyard surrounded by high walls and Site B nestled in a tighter, more enclosed area. The map\'s design offers a mix of open spaces and narrow pathways, encouraging both strategic planning and dynamic engagements. Key areas include Mid, which is crucial for map control, the Temple with its elevated sightlines, the Canals providing sneaky alternative routes, and the Ruins offering cover and ambush opportunities.',
-        mapThumbnail: 'images/anubis.jpg',
-        mapCallouts: 'images/anubiscallouts.jpg',
+        mapThumbnail: 'images/anubis_1.webp',
+        mapCallouts: 'images/anubiscallouts_1.webp',
         mapLineups: ''
 
     },
@@ -12,8 +12,8 @@ const maps = [
         mapIndex: 1,
         mapName: 'Ancient',
         mapDesc: 'Ancient is a relatively newer addition to the competitive pool, featuring a jungle-themed environment with a mix of open spaces and narrow passages. The bomb sites are A, located in an open courtyard, and B, situated in a tighter, enclosed area. Key areas include Mid, Donut, Temple, and Cave.',
-        mapThumbnail: 'images/ancient.jpg',
-        mapCallouts: 'images/ancientcallouts.jpg',
+        mapThumbnail: 'images/ancient_1.webp',
+        mapCallouts: 'images/ancientcallouts_1.webp',
         mapLineups: ''
 
     },
@@ -21,8 +21,8 @@ const maps = [
         mapIndex: 2,
         mapName: 'Dust II',
         mapDesc: 'Dust II is one of the most iconic maps in Counter-Strike history, known for its balanced design and straightforward layout. It features two bomb sites: A and B. The map is favored for its open spaces and long sightlines, which provide opportunities for both sniping and close-quarters combat. Key areas include Long A, Catwalk, Mid, and B Tunnels.',
-        mapThumbnail: 'images/dust2.jpg',
-        mapCallouts: 'images/dust2callouts.jpg',
+        mapThumbnail: 'images/dust2_1.webp',
+        mapCallouts: 'images/dust2callouts_1.webp',
         mapLineups: ''
 
     },
@@ -30,8 +30,8 @@ const maps = [
         mapIndex: 3,
         mapName: 'Inferno',
         mapDesc: 'Inferno is known for its narrow, winding streets and its iconic choke points. It features two bomb sites, A and B, with B located in a tight corner near the banana pathway and A in the open space of the bombsite area. The map\'s key areas include Banana, Apartments, Arch, and Mid.',
-        mapThumbnail: 'images/inferno.jpg',
-        mapCallouts: 'images/infernocallouts.jpg',
+        mapThumbnail: 'images/inferno_1.webp',
+        mapCallouts: 'images/infernocallouts_1.webp',
         mapLineups: ''
 
     },
@@ -39,8 +39,8 @@ const maps = [
         mapIndex: 4,
         mapName: 'Mirage',
         mapDesc: 'Mirage is a well-balanced map that offers a variety of tactical options and engagement distances. With bomb sites located at A in the palace and B near the apartments, players can choose between different strategies like executing smokes, faking, or split attacks. Key areas include Mid, Connector, Jungle, and Underpass.',
-        mapThumbnail: 'images/mirage.jpg',
-        mapCallouts: 'images/miragecallouts.jpg',
+        mapThumbnail: 'images/images/mirage_1.webp',
+        mapCallouts: 'images/miragecallouts_1.webp',
         mapLineups: ''
 
     },
@@ -48,8 +48,8 @@ const maps = [
         mapIndex: 5,
         mapName: 'Nuke',
         mapDesc: 'Nuke is a unique map that offers a vertical element with its multi-layered design, featuring both above-ground and underground bomb sites. The map is known for its complex layout and numerous hiding spots, which require effective communication and coordination. Key areas include Outside, Ramp Room, Heaven, and Vent.',
-        mapThumbnail: 'images/nuke.jpg',
-        mapCallouts: 'images/nukecallouts.png',
+        mapThumbnail: 'images/nuke_1.webp',
+        mapCallouts: 'images/nukecallouts_1.webp',
         mapLineups: ''
 
     },
@@ -57,8 +57,8 @@ const maps = [
         mapIndex: 6,
         mapName: 'Vertigo',
         mapDesc: 'Vertigo offers a unique vertical gameplay experience, with bomb sites located on different floors of a high-rise building under construction. The map is known for its close-quarters combat and precarious ledges. Key areas include Ramp, Mid, Elevator, and Scaffold.',
-        mapThumbnail: 'images/vertigo.jpg',
-        mapCallouts: 'images/vertigocallouts.jpg',
+        mapThumbnail: 'images/vertigo_1.webp',
+        mapCallouts: 'images/vertigocallouts_1.webp',
         mapLineups: ''
 
     }
